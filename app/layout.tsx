ca8c51b2a3385da33df409b10e6aa4d0c9d863ab
cris <gwestonem@gmail.com>
2023-06-26 23:01:56 +0300

    fixed unstyled flash

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import StyledComponentsRegistry from "@/lib/registry";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
@@ -16,10 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>
-        <Navbar />
-        {children}
+    <html>
+      <head>
+        <title>Your App</title>
+      </head>
+      <body style={roboto.style}>
+        <StyledComponentsRegistry>
+          <div>
+            <Navbar />
+            {children}
+          </div>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
+    // Enables the styled-components SWC transform
     styledComponents: true,
+  },
+  swcMinify: true,
+  experimental: {
+    appDir: true,
   },
 };
 
